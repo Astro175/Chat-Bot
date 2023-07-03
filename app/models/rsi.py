51e2f918e923df26b774f007ecd2ec741abb8697
astro175 <aspark400@gmail.com>
2023-07-03 19:44:22 +0100

    added model class user

---
+++ app/models/rsi.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that does rsi signal analysis"""
+
+import pandas as pd 
+import requests
+import numpy as np
+from data_retrieval import data_df
+
+def get
---
+++ app/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+from models.user import User
+
+u = User(1000)
+print("{}".format(u.asset))
