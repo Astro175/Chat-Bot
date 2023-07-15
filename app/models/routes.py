cdab6048aad80943786d019e7407aca1d024f61a
astro175 <aspark400@gmail.com>
2023-07-15 18:30:54 +0100

    added more views

--- app/models/routes.py
+++ app/models/routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template, redirect
 
-from .views import HomePageView, UpdateBinanceSymbolView, WalletView, UpdateWalletAssetView, UpdateCryptoBotSettingsView, ManageCoinAddView, ManageCoinDelView, CryptoDetailView, autocompleteAdd, autocompleteDel, SearchResults, GetOldOhlcvView, StartWebSocketView
-
+from models import HomePageView, UpdateBinanceSymbolView, WalletView, UpdateWalletAssetView, UpdateCryptoBotSettingsView, ManageCoinAddView, ManageCoinDelView, CryptoDetailView, autocompleteAdd, autocompleteDel, SearchResults, GetOldOhlcvView, StartWebSocketView
+from forms import *
+from wallets import *
+import requests
 
 app = Blueprint('app', __name__, template_folder='templates')
 
@@ -11,65 +13,127 @@ def homepage():
     return render_template('homepage.html')
 
 
-@app.route('/wallet.html')
-def wallet():
-    return render_template('wallet.html')
+@app.route("/manage-coin/add", methods=["GET", "POST"])
+def manage_coin_add():
+    form = CoinListAddForm()
+    coinlist = CoinList.objects.all()
+    symbolist = BinanceSymbolList.objects.all()
+    if requests.method == "POST":
+        if form.validate_on_submit():
+            form.save()
+            show_text = True
+        else:
+            show_text = False
+    else:
+        show_text = False
+    return render_template(
+        "manage-coin.html",
+        addform=form,
+        coinlist=coinlist,
+        show_text=show_text,
+        symbolist=symbolist,
+    )
 
 
-@app.route('/manage-coin.html')
-def manage_coin():
-    return render_template('manage-coin.html')
+@app.route("/manage-coin/delete", methods=["GET", "POST"])
+def manage_coin_delete():
+    form = CoinListDelForm()
+    coinlist = CoinList.objects.all()
+    symbolist = BinanceSymbolList.objects.all()
+    if requests.method == "POST":
+        if form.validate_on_submit():
+            show_del_text = True
+        else:
+            show_del_text = False
+    else:
+        show_del_text = False
+    return render_template(
+        "manage-coin.html",
+        delform=form,
+        coinlist=coinlist,
+        symbolist=symbolist,
+        show_del_text=show_del_text,
+    )
 
 
-@app.route('/manage-coin.html/update-binance-list/')
-def update_binance_list():
-    return redirect(UpdateBinanceSymbolView.as_view())
+@app.route("/wallet")
+def wallet_view():
+    wallet_asset = WalletAssetList.objects.all()
+    wallet_balance = WalletAssetBalance.objects.all()
+    return render_template("wallet.html", wallet_asset=wallet_asset, wallet_balance=wallet_balance)
 
 
-@app.route('/manage-coin.html/delete-coin/')
-def delete_coin():
-    return redirect(ManageCoinDelView.as_view())
-
-
-@app.route('/Wallet/Update_Asset/')
+@app.route("/wallet/update-asset", methods=["POST"])
 def update_wallet_asset():
-    return redirect(UpdateWalletAssetView.as_view())
+    form = UpdateWalletAssetForm()
+    if form.validate_on_submit():
+        save_balance = get_binance_savings()
+        btc_save = save_balance["totalAmountInBTC"]
+        usdt_save = save_balance["totalAmountInUSDT"]
+        locked_stake_usdt, locked_stake_btc = get_binance_locked_stacking()
+        flex_defi_stake_usdt, flex_defi_stake_btc = get_binance_flex_defi_stacking()
+        locked_defi_stake_usdt, locked_defi_stake_btc = get_binance_locked_defi_stacking()
+        asset_list, spot_usdt, spot_btc = get_wallet_assets(info)
+        for index, row in asset_list.iterrows():
+            asset = row["asset"]
+            free = row["free"]
+            locked = row["locked"]
+            ownusdt = row["ownusdt"]
+            ownbtc = row["ownbtc"]
+            WalletAssetList.objects.create(
+                free=free, locked=locked, ownusdt=ownusdt, ownbtc=ownbtc, asset=asset
+            )
+        usdt_tot = float(usdt_save) + float(spot_usdt) + float(locked_stake_usdt)
+        btc_tot = float(btc_save) + float(spot_btc) + float(locked_stake_btc)
+        usdt_stake = locked_stake_usdt + flex_defi_stake_usdt + locked_defi_stake_usdt
+        btc_stake = locked_stake_btc + flex_defi_stake_btc + locked_defi_stake_btc
+        WalletAssetBalance.objects.create(
+            usdtspot=spot_usdt,
+            btcspot=spot_btc,
+            usdtsav=usdt_save,
+            btcsav=btc_save,
+            usdtbal=usdt_tot,
+            btcbal=btc_tot,
+            usdtstake=usdt_stake,
+            btcstake=btc_stake,
+        )
+    return redirect(url_for("wallet_view"))
 
 
-@app.route('/settings.html')
-def settings():
-    return render_template('settings.html')
+@app.route("/manage-coin/update-symbol", methods=["GET", "POST"])
+def update_binance_symbol():
+    form = UpdateBnSymbol()
+    coinlist = CoinList.objects.all()
+    symbolist = BinanceSymbolList.objects.all()
+    symbol_list = get_binance_symbol()
+    symbol_list.sort()
+    symbol = BinanceSymbolList.objects.values_list("symbol", flat=True)
+    symbol = list(symbol)
+    symbol.sort()
+    if symbol_list == symbol:
+        list_updated = True
+    else:
+        new = list(set(symbol_list) - set(symbol))
+        BinanceSymbolList.objects.bulk_create([BinanceSymbolList(symbol=x) for x in new])
+        show_list_text = True
+    return render_template(
+        "manage-coin.html",
+        list_updated=list_updated,
+        show_list_text=show_list_text,
+        coinlist=coinlist,
+        symbolist=symbolist,
+    )
 
 
-@app.route('/autocompleteAdd')
-def autocomplete_add():
-    return autocompleteAdd()
-
-
-@app.route('/autocompleteDel')
-def autocomplete_del():
-    return autocompleteDel()
-
-
-@app.route('/dashboard/<slug:slug>/')
-def crypto_details(slug):
-    return render_template('crypto-details.html', slug=slug)
-
-
-@app.route('/search/<str:search>/')
-def search_results(search):
-    return SearchResults(search)
-
-
-@app.route('/dashboard/<str:asset>/get-old-ohlcv/')
-def get_old_ohlcv(asset):
-    return GetOldOhlcvView(asset)
-
-
-@app.route('/dashboard/<str:asset>/start-websocket/')
-def start_websocket(asset):
-    return StartWebSocketView(asset)
-
+@app.route("/settings", methods=["GET", "POST"])
+def update_crypto_bot_settings():
+    form = UpdateCryptoBotSettings()
+    if form.validate_on_submit():
+        form.save()
+        show_text = True
+    else:
+        show_text = False
+    return render_template("settings.html", form=form, show_text=show_text)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ app/models/wallets.py
@@ -0,0 +1,212 @@
+from typing import Any
+from binance.client import Client
+import pandas as pd
+from binance.exceptions import BinanceAPIException
+import requests
+import json
+import hashlib
+from urllib.parse import urlencode
+import hmac
+from models import CoinList, BinanceSymbolList, WalletAssetList
+import os
+
+api_key = os.getenv("api_key")
+api_secret = os.getenv("api_secret")
+
+
+# initialize
+client = Client(api_key, api_secret)
+
+
+# get account info
+info = client.get_account()
+
+
+# get Binance spot
+def get_wallet_assets(info):
+    own_btc_list = []
+    own_usdt_list = []
+    df = pd.DataFrame(info["balances"])
+    df["free"] = df["free"].astype(float).round(4)
+    df_assets = df[df["free"] > 0]
+    pd.options.display.float_format = '{:.4f}'.format
+    df_assets = df_assets.sort_values('asset')
+    sum_usdt = 0.0
+    current_btc_price_USD = client.get_symbol_ticker(symbol="BTCUSDT")["price"]
+    for asset in df_assets['asset']:
+        df = df_assets.loc[df_assets['asset'] == asset]
+        asset_quantity = float(df["free"]) + float(df["locked"])  
+        if asset == "BTC":
+            sum_usdt += asset_quantity * float(current_btc_price_USD)
+            btc_value = asset_quantity * float(current_btc_price_USD)
+            own_btc_list.append(btc_value)
+        else:
+            btc_value = 0.0
+            own_btc_list.append(btc_value)
+            try:
+                _price = client.get_symbol_ticker(symbol=asset + "USDT")
+                sum_usdt += asset_quantity * float(_price["price"])
+                usdt_value =  asset_quantity * float(_price["price"])
+                own_usdt_list.append(usdt_value)
+            except BinanceAPIException:
+                usdt_value = 0.0
+                own_usdt_list.append(usdt_value)
+                continue
+    spot_btc = sum_usdt / float(current_btc_price_USD)
+    spot_usdt = sum_usdt
+    df_assets['ownusdt'] = own_usdt_list
+    df_assets['ownbtc'] = own_btc_list
+    return df_assets, spot_usdt, spot_btc
+
+
+# Get Binance savings   (ADD flexible, locked)
+def get_binance_savings():
+    servertime = requests.get("https://api.binance.com/api/v1/time")
+    servertimeobject = json.loads(servertime.text)
+    servertimeint = servertimeobject['serverTime']
+    params = urlencode({
+        "timestamp" : servertimeint,
+    })
+    hashedsig = hmac.new(api_secret.encode('utf-8'), params.encode('utf-8'),hashlib.sha256).hexdigest()
+    save_balance = requests.get("https://api.binance.com/sapi/v1/lending/union/account",
+    params = {
+        "timestamp" : servertimeint,
+        "signature" : hashedsig,   
+    },
+    headers = {
+        "X-MBX-APIKEY" : api_key,
+    }
+    )
+    save_balance = save_balance.text
+    save_balance = json.loads(save_balance)
+    return save_balance
+
+
+# Get Binance locked staking
+def get_binance_locked_stacking():
+    prod_type = "STAKING"
+    servertime = requests.get("https://api.binance.com/api/v1/time")
+    servertimeobject = json.loads(servertime.text)
+    servertimeint = servertimeobject['serverTime']
+    params = urlencode({
+        "timestamp" : servertimeint,
+        "product" : prod_type,
+    })
+    hashedsig = hmac.new(api_secret.encode('utf-8'), params.encode('utf-8'),hashlib.sha256).hexdigest()
+    stake_balance = requests.get("https://api.binance.com/sapi/v1/staking/position",
+        params = {
+            "timestamp" : servertimeint,
+            "product" : prod_type, 
+            "signature" : hashedsig,
+        
+        },
+        headers = {
+            "X-MBX-APIKEY" : api_key,
+        }
+    )
+    locked_stake_usdt = 0
+    locked_stake_btc = 0
+    lock_stake_asset = json.loads(stake_balance.text)
+    for item in lock_stake_asset:
+        asset = item['asset']
+        asset_amount = item['amount']
+        asset_amount = float(asset_amount)
+        asset_usdt_price = client.get_symbol_ticker(symbol=asset + "USDT")
+        asset_usdt_price = asset_usdt_price['price']
+        locked_stake_usd = asset_amount * float(asset_usdt_price)
+        locked_stake_usdt = locked_stake_usd + locked_stake_usdt
+        btc_price = client.get_symbol_ticker(symbol="BTCUSDT")["price"]
+        locked_stake_in_btc = asset_amount * float(btc_price)
+        locked_stake_btc = locked_stake_in_btc + locked_stake_btc    
+    locked_stake_btc = locked_stake_usdt / float(btc_price)
+    return locked_stake_usdt, locked_stake_btc
+
+
+# Get Binance flexible defi staking
+def get_binance_flex_defi_stacking():
+    prod_type = "F_DEFI"
+    servertime = requests.get("https://api.binance.com/api/v1/time")
+    servertimeobject = json.loads(servertime.text)
+    servertimeint = servertimeobject['serverTime']
+    params = urlencode({
+        "timestamp" : servertimeint,
+        "product" : prod_type,
+    })
+    hashedsig = hmac.new(api_secret.encode('utf-8'), params.encode('utf-8'),hashlib.sha256).hexdigest()
+    flex_defi = requests.get("https://api.binance.com/sapi/v1/staking/position",
+        params = {
+            "timestamp" : servertimeint,
+            "product" : prod_type, 
+            "signature" : hashedsig,
+        
+        },
+        headers = {
+            "X-MBX-APIKEY" : api_key,
+        }
+    )
+    flex_defi_stake_usdt = 0
+    flex_defi_stake_btc = 0
+    flex_defi_stake_asset = json.loads(flex_defi.text)
+    if not flex_defi_stake_asset:
+        flex_defi_stake_usdt = 0.0
+        flex_defi_stake_btc = 0.0
+        return flex_defi_stake_usdt, flex_defi_stake_btc
+    for item in flex_defi_stake_asset:
+        asset = item['asset']
+        asset_amount = item['amount']
+        asset_amount = float(asset_amount)
+        asset_usdt_price = client.get_symbol_ticker(symbol=asset + "USDT")
+        asset_usdt_price = asset_usdt_price['price']
+        flex_defi_stake_usd = asset_amount * float(asset_usdt_price)
+        flex_defi_stake_usdt = flex_defi_stake_usd + flex_defi_stake_usdt
+        btc_price = client.get_symbol_ticker(symbol="BTCUSDT")["price"]
+        print("BTC price")
+        print(btc_price)
+        flex_defi_stake_in_btc = asset_amount * float(btc_price)
+        flex_defi_stake_btc = flex_defi_stake_in_btc + flex_defi_stake_btc    
+    flex_defi_stake_btc = flex_defi_stake_usdt / float(btc_price)
+    return flex_defi_stake_usdt, flex_defi_stake_btc
+
+
+# Get Binance locked defi staking 
+def get_binance_locked_defi_stacking():
+    prod_type = "L_DEFI"
+    servertime = requests.get("https://api.binance.com/api/v1/time")
+    servertimeobject = json.loads(servertime.text)
+    servertimeint = servertimeobject['serverTime']
+    params = urlencode({
+        "timestamp" : servertimeint,
+        "product" : prod_type,
+    })
+    hashedsig = hmac.new(api_secret.encode('utf-8'), params.encode('utf-8'),hashlib.sha256).hexdigest()
+    locked_defi = requests.get("https://api.binance.com/sapi/v1/staking/position",
+        params = {
+            "timestamp" : servertimeint,
+            "product" : prod_type, 
+            "signature" : hashedsig,
+        
+        },
+        headers = {
+            "X-MBX-APIKEY" : api_key,
+        }
+    )
+    locked_defi_stake_usdt = 0
+    locked_defi_stake_btc = 0
+    locked_defi_stake_asset = json.loads(locked_defi.text)
+    if not locked_defi_stake_asset:
+        locked_defi_stake_usdt = 0.0
+        locked_defi_stake_btc = 0.0
+        return locked_defi_stake_usdt, locked_defi_stake_btc
+    for item in locked_defi_stake_asset:
+        asset = item['asset']
+        asset_amount = item['amount']
+        asset_amount = float(asset_amount)
+        asset_usdt_price = client.get_symbol_ticker(symbol=asset + "USDT")
+        asset_usdt_price = asset_usdt_price['price']
+        locked_defi_stake_usd = asset_amount * float(asset_usdt_price)
+        locked_defi_stake_usdt = locked_defi_stake_usd + locked_defi_stake_usdt
+        btc_price = client.get_symbol_ticker(symbol="BTCUSDT")["price"]
+        locked_defi_stake_in_btc = asset_amount * float(btc_price)
+        locked_defi_stake_btc = locked_defi_stake_in_btc + locked_defi_stake_btc    
+    locked_defi_stake_btc = locked_defi_stake_usdt / float(btc_price)
+    return locked_defi_stake_usdt, locked_defi_stake_btc
