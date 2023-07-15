58126079b89a0cb65c722b1ea298ec6782a4a0cb
astro175 <aspark400@gmail.com>
2023-07-15 12:16:19 +0100

    changed data_retrieval file

--- app/models/data_retrieval.py
+++ app/models/data_retrieval.py
@@ -1,38 +1,93 @@
-import requests
+from typing import Any
+from binance.client import Client
 import pandas as pd
-import time
+import datetime as dt
+import config
+from binance.exceptions import BinanceAPIException
+from flask import current_app
+from models import CoinList, BinanceSymbolList, Ohlcv
+import os
+from sqlalchemy import create_engine
 
-"""
-Module that retrieves data from binance api into
-a panda dataframe
-"""
-endpoint = 'https://api.binance.com/api/v3/klines'
+api_key = os.getenv("api_key")
+api_secret = os.getenv("api_secret")
 
-symbol = 'BTCUSDT'
-interval = '1d'
+# initialize
+client = Client(api_key, api_secret)
 
-params = {
-    'symbol': symbol,
-    'interval': interval,
-    'limit': 1000,  
-    'startTime': 1672531200000,
-    'endTime': int(time.time() * 1000),
-}
 
-def retrieve_data():
-    """Sends an API request, convert the response
-       to a pandas dataframe and convert the timestamp
-    """
-    response = requests.get(endpoint, params=params)
-    data = response.json()
+# DB connection
+user = config.DATABASES['default']['USER']
+password = config.DATABASES['default']['PASSWORD']
+database_name = config.DATABASES['default']['NAME']
+host_name = config.DATABASES['default']['HOST']
 
-    df = pd.DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time',
-                                     'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume',
-                                     'taker_buy_quote_asset_volume', 'ignore'])
+database_url = 'mysql+mysqldb://{user}:{password}@{host_name}/{database_name}'.format(
+    user=user,
+    password=password,
+    database_name=database_name,
+    host_name = host_name,
+)
 
-    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-    df['close'] = df['close'].astype(float)
 
-    return df
+engine = create_engine(database_url, echo=False)
 
-data_df = retrieve_data()
+
+
+
+# Get all Binance symbols
+def get_binance_symbol():
+    symbol_list = []
+    exchange_info = client.get_exchange_info()
+    for n in exchange_info['symbols']:
+        symbol = n['symbol']
+        symbol_list.append(symbol)
+    return symbol_list    
+
+
+
+    
+
+
+
+
+
+
+
+def get_old_ohlcv(slug):
+    # obj = CoinList.query.filter_by(coin=slug,price__isnull=True).first()
+    # if obj:
+    #     past_days = 5
+    #     status = True
+    # else:
+    #     past_days = 1
+    #     status = False  
+
+    past_days = 20
+    # client = Client()
+    interval = '4h'
+    start_str = str(
+        (pd.to_datetime('today')-pd.Timedelta(str(past_days)+' days')).date())
+    D = pd.DataFrame(client.get_historical_klines(
+        symbol=slug, start_str=start_str, interval=interval))
+    D.columns = ['open_time', 'open', 'high', 'low', 'close', 'volume',
+                 'close_time', 'qav', 'num_trades', 'taker_base_vol',
+                 'taker_quote_vol', 'is_best_match']
+    D['date'] = [
+        dt.datetime.fromtimestamp(x/1000) for x in D.open_time]
+    dfohlcv = D[['date', 'open', 'high', 'low', 'close',
+                 'volume', 'num_trades', 'taker_base_vol', 'taker_quote_vol']]
+    D['coin_id'] = slug
+    dfohlcv = D[['date', 'open', 'high', 'low', 'close',
+                 'volume', 'num_trades', 'taker_base_vol', 'taker_quote_vol', 'coin_id']]
+    print(dfohlcv)
+   
+    dfohlcv.to_sql(Ohlcv.__tablename__, con=engine, if_exists='append', index=False)
+
+    # Ohlcv.query.update( \
+    #             values={'free': free, 'locked': locked, \
+    #             'ownusdt': ownusdt, 'ownbtc': ownbtc}, \
+    #             synchronize_session=False )                       
+
+    return True
+
