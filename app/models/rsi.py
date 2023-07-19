54dba440d02885d3de8563ab5e37966e340e5103
astro175 <aspark400@gmail.com>
2023-07-19 22:44:20 +0100

    switched framework from flask to django at late minutes...

--- app/models/rsi.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Module that does rsi signal analysis"""
-
-import pandas as pd 
-import requests
-import numpy as np
-from data_retrieval import data_df
-
-def get
--- app/test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-from models.user import User
-
-u = User(1000)
-print("{}".format(u.asset))
